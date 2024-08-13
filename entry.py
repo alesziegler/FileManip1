c0ad160579b9d7d1a805e042f9793f7a09a852e6
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-08-13 13:26:56 +0000

    class "entry" added

--- entry.py
+++ entry.py
@@ -1,2 +1,10 @@
 class Entry:
   content = "something"
+
+  def __init__(self,name, property1, property2):
+    self.name = name
+    self.property1 = property1
+    self.property2 = property2
+
+  def __str__(self):
+    return self.name
