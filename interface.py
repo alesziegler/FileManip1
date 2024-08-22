9b4588057939d46a758dbd11dfde06ac9dbf8c17
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-08-22 10:32:12 +0000

    menu created

--- interface.py
+++ interface.py
@@ -1,12 +1,41 @@
-
 class Interface:
   welcome = "this will be a menu"
 
   def __init__(self):
-   self.menu()
+    self.menu()
 
   def menu(self):
-    print("this")
+    done = False
 
-  def __str__(self):
-    return "This one"
+    while not done:
+
+      print('''
+          1) add entry to the database.
+          2) show the whole database.
+          3) save.
+          4) load.
+          5) quit.
+        ''')
+      choice = input("Pick what do you want to do: ")
+
+      try:
+        choice = int(choice)
+      except ValueError:
+        print("invalid choice")
+        continue
+        
+
+      match choice:
+        case 1:
+          print("s")
+        case 2:
+          print(2)
+        case 3:
+          print(3)
+        case 4:
+          print(4)
+        case 5:
+          print("bye")
+          done = True
+        case _:
+          print("invalid choice")
--- main.py
+++ main.py
@@ -5,17 +5,6 @@ from interface import Interface #as interface
 
 print("Hello")
 
-print(entry.content)
-
-#data = input("Add here: ")
-
 Interface()
 
-#print(Interface.menu)
-
-#Interface.menu
-
-#print(Interface())
-
-print(Interface.welcome)
 
